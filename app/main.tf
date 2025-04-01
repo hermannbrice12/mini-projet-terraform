4cca55d24ccaa133f994ba4aaab6f8b52626c1d9
hermannbrice12 <165166673+hermannbrice12@users.noreply.github.com>
2025-04-01 18:54:15 +0200

    Update main.tf

--- app/main.tf
+++ app/main.tf
@@ -1,51 +1,45 @@
-data "aws_ami" "ubuntu_ami" {
-  most_recent = true
-  owners = [ "099720109477" ]
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+module "keypair" {
+  source = "../modules/keypair"
+  key_name = var.key_name
 }
 
-module "securitygroup" {
-  source = "../modules/securitygroup"
-  security_group_name = "devops_sg"
+module "sg" {
+  source              = "../modules/sg"
+  security_group_name = var.security_group_name
+  security_group_tag  = var.security_group_tag
 }
 
 module "ec2" {
-  source = "../modules/ec2"
-  tag_name = "devops_ec2"
-  security_group_name = module.securitygroup.security_group_name
-  user = "ubuntu"
-  key_name = "devops"
-  availability_zone = "us-east-1a"
-  ami = 
+  source              = "../modules/ec2"
+  instance_type       = var.instance_type
+  key_name            = var.key_name
+  security_group_name = module.sg.security_group_name
+  tags                = var.instance_tag
+  user                = var.user
+  availability_zone = module.ebs.ebs_zone
 
 }
 
 module "eip" {
-  source = "../modules/eip"
-  eip_name = "devops_eip"
+  source  = "../modules/eip"
+  eip_tag = var.eip_tag
 }
 
 module "ebs" {
-  source = "../modules/ebs"
-  ebs_size = 10
-  ebs_zone = "us-east-1a"
-  ebs_tags = "devops_ebs"
+  source   = "../modules/ebs"
+  ebs_zone = var.ebs_zone
+  ebs_size = var.ebs_size
+  ebs_tag  = var.ebs_tag
 }
 
+
 resource "aws_eip_association" "eip_association" {
-  instance_id = module.ec2.ec2_id
+  instance_id   = module.ec2.ec2_id
   allocation_id = module.eip.eip_id
 }
 
-resource "aws_volume_attachment" "ebs_att" {
+resource "aws_volume_attachment" "ebs_attachment" {
   device_name = "/dev/sdh"
+  volume_id   = module.ebs.ebs_id
   instance_id = module.ec2.ec2_id
-  volume_id = module.ebs.ebs_id
 }
