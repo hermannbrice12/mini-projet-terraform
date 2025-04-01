96c7f504353b660176dc898827acc56169cb9453
hermannbrice12 <165166673+hermannbrice12@users.noreply.github.com>
2025-04-01 19:02:37 +0200

    Update variable.tf

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -1,21 +1,33 @@
-variable "ami" {
-  
-}
 variable "instance_type" {
-  
+  type = string 
+  default = "t2.medium"
+  description = "Instance type for the EC2 instance"
 }
 variable "availability_zone" {
   
 }
-variable "user" {
-  
-}
 variable "key_name" {
-  
+    type = string
+    default = "projet-terraform"
+    description = "Key pair name for the EC2 instance"
 }
+
+variable "tags" {
+  type = map 
+  default = {
+    "Name" = "ec2"
+  }
+  description = "Tags for the EC2 instance"
+}
+
 variable "security_group_name" {
-  
+  type = string 
+  default = "security_group"
+  description = "Name of the security group"
 }
-variable "tag_name" {
-  
+
+variable "user" {
+    type = string
+    default = "ubuntu"
+    description = "User for the EC2 instance"
 }
