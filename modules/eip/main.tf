dbcdfddd40a45ed3aec34ff5aa13d276d86e3201
hermannbrice12 <165166673+hermannbrice12@users.noreply.github.com>
2025-04-01 19:03:52 +0200

    Update main.tf

--- modules/eip/main.tf
+++ modules/eip/main.tf
@@ -1,8 +1,7 @@
 resource "aws_eip" "eip" {
+  # domain = "vpc"
   provisioner "local-exec" {
-    command = "echo ${aws_eip.eip.public_ip} > ip_address.txt"
+    command = "echo The public IP address is ${aws_eip.eip.public_ip} >> ip_ec2.txt"
   }
-  tags = {
-    Name = "var.eip_name"
-  }
+  tags = var.eip_tag
 }
